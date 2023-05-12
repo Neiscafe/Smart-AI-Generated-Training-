c00719423917e6ddd8ee5d7b4aafee45f5d65c99
Neiscafe <110692081+Neiscafe@users.noreply.github.com>
2023-05-12 15:37:33 -0300

    commit

---
+++ app/src/main/java/com/example/smart_development/ui/screens/MainScreen.kt
@@ -0,0 +1,19 @@
+package com.example.smart_development.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+    Column {
+        Text(text = )
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+
+}
